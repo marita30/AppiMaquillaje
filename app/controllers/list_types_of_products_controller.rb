756b773e8d0d5e66a3a0f46b3f59c5277ed6920e
marita30 <marita.jose.ch@gmail.com>
2019-07-09 23:30:44 +0200

    Agregando dos categorias mas , una bronzer y la otra eyebrow

--- app/controllers/list_types_of_products_controller.rb
+++ app/controllers/list_types_of_products_controller.rb
@@ -6,6 +6,8 @@ class ListTypesOfProductsController < ApplicationController
 
     def show
         @blush_categories = []
+        @bronzer_categories = []
+        @eyebrow_categories = []
         response = RestClient.get 'http://makeup-api.herokuapp.com/api/v1/products.json? '
         # para desahacer el texto plano
         @array = JSON.parse(response.body)
@@ -14,11 +16,20 @@ class ListTypesOfProductsController < ApplicationController
         @array.each do |value|
             if value['product_type'] == 'blush' 
                 @blush_categories << value['category']
+
+            elsif value['product_type'] == 'bronzer'
+                @bronzer_categories << value['category'] 
+            
+            elsif value['product_type'] == 'eyebrow'
+                @eyebrow_categories << value['category'] 
+
             end
 
         end
-        # para hacer el valor unico , que no se repita y el reject es para eliminar valores en blanco o nill
+        
         @blush_categories =  @blush_categories.uniq { |category| category }.reject(&:blank?)
+        @bronzer_categories =  @bronzer_categories.uniq { |category| category }.reject(&:blank?)
+        @eyebrow_categories =  @eyebrow_categories.uniq { |category| category }.reject(&:blank?)
        
         binding.pry
         
