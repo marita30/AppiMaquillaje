7c8ec5e3273820019cc3d7193a05bf5b3d00848d
marita30 <marita.jose.ch@gmail.com>
2019-07-09 22:51:26 +0200

    añadiendo una vista y agregando la gem pry and en el controller agregando el primer array con la primer categoria blush con sus subcategorias

--- app/controllers/list_types_of_products_controller.rb
+++ app/controllers/list_types_of_products_controller.rb
@@ -1,2 +1,30 @@
+require 'rest-client'
+require 'pry'
+require 'json'
+
 class ListTypesOfProductsController < ApplicationController
+
+    def show
+        @blush_categories = []
+        response = RestClient.get 'http://makeup-api.herokuapp.com/api/v1/products.json? '
+        # para desahacer el texto plano
+        @array = JSON.parse(response.body)
+
+
+        @array.each do |value|
+            if value['product_type'] == 'blush' 
+                @blush_categories << value['category']
+            end
+
+        end
+        # para hacer el valor unico , que no se repita y el reject es para eliminar valores en blanco o nill
+        @blush_categories =  @blush_categories.uniq { |category| category }.reject(&:blank?)
+       
+        binding.pry
+        
+    end
+
+    
+
+    
 end
