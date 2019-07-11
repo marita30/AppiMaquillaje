b21b3b61cbe6ee6800789fd5ff0a322fa594bede
marita30 <marita.jose.ch@gmail.com>
2019-07-11 18:16:47 +0200

    Agregando las ultimas rutas y productos en las vistas

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get '/category/Bronzer', to: 'list_types_of_products#show'
   get '/category/EyeBrow', to: 'list_types_of_products#show'
   get '/category/EyeLiner', to: 'list_types_of_products#show'
+  get '/category/EyeShadow', to: 'list_types_of_products#show'
+  get '/category/Foundation', to: 'list_types_of_products#show'
+  get '/category/LipLiner', to: 'list_types_of_products#show'
+  get '/category/Mascara', to: 'list_types_of_products#show'
 
 end
