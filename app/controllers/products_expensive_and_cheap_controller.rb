507afe04eefaeaf654252d4b4bbe8c2eed031e1b
marita30 <marita.jose.ch@gmail.com>
2019-07-12 19:53:26 +0200

    Agregando sort -- que es para ardenar el precio de las categorias de todos los productos de maquillaje

--- app/controllers/products_expensive_and_cheap_controller.rb
+++ app/controllers/products_expensive_and_cheap_controller.rb
@@ -7,16 +7,21 @@ class ProductsExpensiveAndCheapController < ApplicationController
     def index
         @type = params["type"]
         @category =params["category"]
+        @Information = find(@type,@category)
 
        
     end
 
-    # def find(Product, Category)
-    #     value = params["element"]
+    def find(type, category)
+        response = RestClient.get "http://makeup-api.herokuapp.com/api/v1/products.json?product_type=#{type}&product_category=#{category}"
+        @array = JSON.parse(response.body)
+            # Para arreglar el precio de los productos
+           @array = @array.sort_by{ |value| value["price"].to_f}
+        
 
-    #     response = RestClient.get "http://makeup-api.herokuapp.com/api/v1/products.json?product_type=#{value}&product_category=#{value}"
+    
 
-    # end
+    end
 
 
 end
