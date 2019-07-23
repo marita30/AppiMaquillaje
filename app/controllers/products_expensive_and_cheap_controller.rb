9bfbf92a446501382b6414f15ab96423dbe23515
marita30 <marita.jose.ch@gmail.com>
2019-07-23 18:06:14 +0200

    Agregando mas diseño en el scss y en los html , y agregando nuevas fotos en la carpeta images

--- app/controllers/products_expensive_and_cheap_controller.rb
+++ app/controllers/products_expensive_and_cheap_controller.rb
@@ -12,10 +12,12 @@ class ProductsExpensiveAndCheapController < ApplicationController
        
     end
 
+    # metodo para asar los parametros y arreglar los prductos para luego sacar los mas caros y lo mas baratos.
+
     def find(type, category)
         response = RestClient.get "http://makeup-api.herokuapp.com/api/v1/products.json?product_type=#{type}&product_category=#{category}"
         @array = JSON.parse(response.body)
-            # Para ardenar el precio de los productos y corriendo el arreglo como .each
+            # Para ordenar el precio de los productos y corriendo el arreglo como .each
            @array = @array.sort_by{ |value| value["price"].to_f}.reverse!
            @infor = "Product #{type}, Category #{category}" 
         
