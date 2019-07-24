11901e9b9a18d66dab20b1810b455f3a336e1009
marita30 <marita.jose.ch@gmail.com>
2019-07-24 21:45:37 +0200

    agregando a la vista index.html.erb para imprimir el nombre del usuario cuando ya este loguiado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 #  Para agregar un nuevo campo en devise en la database
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -22,4 +22,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
 
   end
+
+
+  
 end
