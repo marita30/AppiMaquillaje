4a1af02cea6ef1b52c50484cfa4e0e49abd76333
marita30 <marita.jose.ch@gmail.com>
2019-07-24 18:32:35 +0200

    Agregando una nueva columna a la base de datos de devise con el comando rails generate migration add_name_to_users name:string.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190723162614) do
+ActiveRecord::Schema.define(version: 20190724162922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190723162614) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
