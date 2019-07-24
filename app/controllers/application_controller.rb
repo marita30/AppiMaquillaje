5abaa4367a8aaa11b43214359cf98a123aa4f1b6
marita30 <marita.jose.ch@gmail.com>
2019-07-24 19:56:33 +0200

    Añadeindo el name en el signUp en la vista y para que la lea devise ponemos un metodo en el archivo application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
+
+#  Para agregar un nuevo campo en devise en la database
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+# Para redireccionar despues del sign in a la ruta types
+
   def after_sign_in_path_for(resource_or_scope)
     types_path
+  end
+
+#  Para agregar un nuevo campo en devise en la database
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
 
 
+
   get '/category/Blush', to: 'list_types_of_products#show'
   get '/category/Bronzer', to: 'list_types_of_products#show'
   get '/category/EyeBrow', to: 'list_types_of_products#show'
