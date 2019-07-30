557734062631f07d243670d1798efa0e700e8b9d
marita30 <marita.jose.ch@gmail.com>
2019-07-30 17:45:18 +0200

    Agregando nuevas funcionalidades

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,23 +1,26 @@
 // Para que el boton cambien sin que se refresh la pagina
 $(document).ready(function(){
     $('.box').click(function(){
+      var id_api = $('.box').text();
+      console.log (id_api)
        
       if ($(this).text()==' Like'){
         $(this).removeClass('entypo-thumbs-up').addClass('entypo-thumbs-down').text(' Unlike');
       }else{
         $(this).removeClass('entypo-thumbs-down').addClass('entypo-thumbs-up').text(' Like');
       }
+
+      // $.ajax({
+      //   type:"POST",
+      //   url:"'/create/like'",
+      //   data:{id_api:id}
+      //   success: function(submit){
+      //       alert(text);
+      //       }
+      //   })
     });
   });
 
-        // $.ajax({
-        //     type:"POST",
-        //     url:"",
-        //     data:{nombre:nombre}
-        //     typedata:text,
-        //     success: function(text){
-        //         alert(text);
-        //         }
-        //     })
+       
      
  
